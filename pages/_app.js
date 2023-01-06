29e7bae02600bc60a287dfdc7eeb1c634dfe7663
techming <hmeng29@wisc.edu>
2023-01-05 21:37:35 -0600

    chore: clean code

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import "../styles/globals.css";
+import "../styles/main.css";
 import 'react-medium-image-zoom/dist/styles.css';
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 import Head from "next/head";
-import Image from "next/image";
 import Footer from "../components/Footer";
 import Featured from "../components/Home/Featured";
 import Grid from "../components/Home/Grid";
 import Heading from "../components/Home/Heading";
 import NavBar from "../components/NavBar";
-import styles from "../styles/Home.module.css";
 import { baseDescription } from "../config";
 import { title } from "../utils";
 
